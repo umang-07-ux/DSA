48cecea0641a975030e8f1864436ef050e835831
Umang Srivastava <umang2897@gmail.com>
2026-07-26 12:41:56 +0530

    Time: 2 ms (99.61%) | Memory: 47.5 MB (82.01%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -1,32 +1,32 @@
 class Solution {
     public int maximumProduct(int[] nums) {
         int n=nums.length;
-        int n1=Integer.MIN_VALUE,n2=Integer.MIN_VALUE,n3=Integer.MIN_VALUE;
-        int m1=Integer.MAX_VALUE,m2=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE,secondmax=Integer.MIN_VALUE,thirdmax=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE,secondmin=Integer.MAX_VALUE;
         for(int i=0;i<n;i++) {
             int x=nums[i];
-            if(x > n1){
-                n3 = n2;
-                n2 = n1;
-                n1 = x;
+            if(x > max){
+                thirdmax = secondmax;
+                secondmax = max;
+                max = x;
             }
-            else if(x > n2){
-                n3 = n2;
-                n2 = x;
+            else if(x > secondmax){
+                thirdmax = secondmax;
+                secondmax = x;
             }
-            else if(x > n3){
-                n3 = x;
+            else if(x > thirdmax){
+                thirdmax = x;
             }
-            if(x < m1){
-                m2 = m1;
-                m1 = x;
+            if(x < min){
+                secondmin = min;
+                min = x;
             }
-            else if(x < m2){
-                m2 = x;
+            else if(x < secondmin){
+                secondmin = x;
             }
         }
-        int product1 = m1 * m2 * n1;
-        int product2 = n1 * n2 * n3;
+        int product1 = min * secondmin * max;
+        int product2 = max * secondmax * thirdmax;
 
         return Math.max(product1, product2);
     }
