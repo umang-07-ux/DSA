fa91dbf0c48fee97ba94d4d39833a53c67b9c1a1
Umang Srivastava <umang2897@gmail.com>
2026-08-23 16:56:01 +0530

    Time: 0 ms (100.00%) | Memory: 80.4 MB (22.34%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n=arr.length;
+        int low=0,high=n-1;
+        int index=-1;
+        while(low<=high) {
+            int mid=(low+high)/2;
+            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]) {
+                index=mid;
+                break;
+            }
+            else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]) {
+                low=mid;
+            }
+            else if(arr[mid]<arr[mid-1] && arr[mid]>arr[mid+1]) {
+                high=mid;
+            }
+            
+           
+        }
+        return index;
+    }
+}
