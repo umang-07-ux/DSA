501329ce68b62711bc1d55087f228300432b58c2
Umang Srivastava <umang2897@gmail.com>
2026-09-01 15:12:25 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (47.32%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -8,7 +8,7 @@ class Solution {
                 return mid;
             }
             else if(nums[low]<=nums[mid]) {   //low to mid is sorted.
-                if(nums[low]<=target && target<nums[mid]) {
+                if(nums[low]<=target && target<=nums[mid]) {
                     high=mid-1;
                 }
                 else {
@@ -16,7 +16,7 @@ class Solution {
                 }
             }
             else {  //mid to high is sorted.
-                if(nums[mid]<target && target<=nums[high]) {
+                if(nums[mid]<=target && target<=nums[high]) {
                     low=mid+1;
                 }
                 else {
