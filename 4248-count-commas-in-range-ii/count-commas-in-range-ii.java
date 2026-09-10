104f201ea4756fdddeeceeab93ec19b41425d453
Umang Srivastava <umang2897@gmail.com>
2026-09-10 13:41:35 +0530

    Time: 1 ms (99.24%) | Memory: 42.7 MB (38.78%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -1,8 +1,21 @@
 class Solution {
     public long countCommas(long n) {
         long ans = 0;
-        for(long i = 1000; i <= n; i *= 1000) {
-            ans += n - i + 1;
+
+        if (n >= 1000) {
+            ans += n - 1000 + 1;
+        }
+        if (n >= 1000000) {
+            ans += n - 1000000 + 1;
+        }
+        if (n >= 1000000000) {
+            ans += n - 1000000000 + 1;
+        }
+        if (n >= 1000000000000L) {
+            ans += n - 1000000000000L + 1;
+        }
+        if (n >= 1000000000000000L) {
+            ans += n - 1000000000000000L + 1;
         }
         return ans;
     }
