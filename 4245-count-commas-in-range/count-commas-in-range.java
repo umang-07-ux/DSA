d005bfc944e379a2a2c9a691e54134d4fef70a2c
Umang Srivastava <umang2897@gmail.com>
2026-09-10 13:38:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (38.67%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -4,10 +4,6 @@ class Solution {
         if(n<=999) {
             return 0;
         }
-        int count=0;
-        for(int i=1000;i<=n;i++) {
-            count++;
-        }
-        return count;
+        return n-1000+1;
     }
 }
